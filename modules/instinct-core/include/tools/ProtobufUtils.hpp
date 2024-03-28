fbe17906d051c0d9e81a1e5c416102e977b220ab
Robin Qu <robinqu@gmail.com>
2024-03-28 16:02:27 +0800

    chores: fix proto bugs

--- modules/instinct-core/include/tools/ProtobufUtils.hpp
+++ modules/instinct-core/include/tools/ProtobufUtils.hpp
@@ -24,7 +24,10 @@ namespace INSTINCT_CORE_NS {
         requires is_pb_message<T>
         static T Deserialize(const std::string& buf) {
             T result;
-            auto status = google::protobuf::util::JsonStringToMessage(buf, &result);
+            auto status = util::JsonStringToMessage(buf, &result);
+            if (!status.ok()) {
+                LOG_DEBUG("Deserialize failed string: {}", buf);
+            }
             assert_true(status.ok(), "failed to parse protobuf message from response body");
             return result;
         }
@@ -33,9 +36,12 @@ namespace INSTINCT_CORE_NS {
         requires is_pb_message<T>
         static std::string Serialize(const T& obj) {
             std::string param_string;
-            google::protobuf::util::JsonPrintOptions json_print_options;
+            util::JsonPrintOptions json_print_options;
             json_print_options.preserve_proto_field_names = true;
-            auto status = google::protobuf::util::MessageToJsonString(obj, &param_string, json_print_options);
+            auto status = util::MessageToJsonString(obj, &param_string, json_print_options);
+            if (!status.ok()) {
+                LOG_DEBUG("Serialize failed message obj: {}", obj.DebugString());
+            }
             assert_true(status.ok(), "failed to dump parameters from protobuf message");
             return param_string;
         }
