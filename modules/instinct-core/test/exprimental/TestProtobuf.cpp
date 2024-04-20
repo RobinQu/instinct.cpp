bbd9e44b8c3fea0c4f1633f732934803633c28fd
RobinQu <robinqu@gmail.com>
2024-04-20 20:07:31 +0800

    chores: proto objects for Assistant, Thread

---
+++ modules/instinct-core/test/exprimental/TestProtobuf.cpp
@@ -0,0 +1,20 @@
+//
+// Created by RobinQu on 2024/4/20.
+//
+#include <gtest/gtest.h>
+#include <assistant_api_v2.pb.h>
+#include <google/protobuf/util/json_util.h>
+
+namespace INSTINCT_CORE_NS {
+    TEST(ProtobufTest, JSONConversion) {
+        instinct::agent::assistant::v2::AssistantObject ao;
+        auto fields = ao.mutable_metadata()->mutable_fields();
+        google::protobuf::Value v;
+        v.set_bool_value(false);
+        fields->emplace("k", v);
+        std::string output;
+        google::protobuf::util::MessageToJsonString(ao, &output);
+        std::cout << output << std::endl;
+        ASSERT_EQ(R"""({"metadata":{"k":false}})""", output);
+    }
+}
