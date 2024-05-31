cf2b177e8aa7a642443a40674423f6f7a7bad40a
RobinQu <robinqu@gmail.com>
2024-05-31 16:21:43 +0800

    test: TestSQLBuilder

--- modules/instinct-retrieval/include/store/SQLBuilder.hpp
+++ modules/instinct-retrieval/include/store/SQLBuilder.hpp
@@ -14,6 +14,62 @@ namespace INSTINCT_RETRIEVAL_NS {
         static void build_term_query(const TermQuery& term_query, std::string& sql);
         static void build_terms_query(const TermsQuery& terms_query, std::string& sql);
         static void build_search_query(const SearchQuery& search_query, std::string& sql);
+        static void build_int_range_query(const IntRangeQuery& range_query, std::string& sql);
+        static void build_double_range_query(const DoubleRangeQuery& range_query, std::string& sql);
+
+        static void build_int_range_query(const IntRangeQuery& range_query, std::string& sql) {
+            std::vector<std::string> ranges;
+            if (range_query.has_from()) {
+                std::string range;
+                range += range_query.name();
+                range += range_query.inclusive_start() ? " >= " : " > ";
+                range += std::to_string(range_query.from());
+                ranges.push_back(range);
+            }
+            if (range_query.has_to()) {
+                std::string range;
+                range += range_query.name();
+                range += range_query.inclusive_end() ? " <= " : " < ";
+                range += std::to_string(range_query.to());
+                ranges.push_back(range);
+            }
+            if (!ranges.empty()) {
+                if (ranges.size()>1) {
+                    sql += "(";
+                    sql += StringUtils::JoinWith(ranges, " AND ");
+                    sql += ")";
+                } else {
+                    sql += ranges[0];
+                }
+            }
+        }
+
+        static void build_double_range_query(const DoubleRangeQuery& range_query, std::string& sql) {
+            std::vector<std::string> ranges;
+            if (range_query.has_from()) {
+                std::string range;
+                range += range_query.name();
+                range += range_query.inclusive_start() ? " >= " : " > ";
+                range += fmt::format("{}", range_query.from());
+                ranges.push_back(range);
+            }
+            if (range_query.has_to()) {
+                std::string range;
+                range += range_query.name();
+                range += range_query.inclusive_end() ? " <= " : " < ";
+                range += fmt::format("{}", range_query.to());
+                ranges.push_back(range);
+            }
+            if (!ranges.empty()) {
+                if (ranges.size()>1) {
+                    sql += "(";
+                    sql += StringUtils::JoinWith(ranges, " AND ");
+                    sql += ")";
+                } else {
+                    sql += ranges[0];
+                }
+            }
+        }
 
         static void build_search_query(const SearchQuery& search_query, std::string& sql) {
             if (search_query.has_bool_()) {
@@ -22,6 +78,10 @@ namespace INSTINCT_RETRIEVAL_NS {
                 build_term_query(search_query.term(), sql);
             } else if(search_query.has_terms()) {
                 build_terms_query(search_query.terms(), sql);
+            } else if(search_query.has_double_range()) {
+                build_double_range_query(search_query.double_range(), sql);
+            } else if(search_query.has_int_range()) {
+                build_int_range_query(search_query.int_range(), sql);
             }
         }
 
@@ -56,7 +116,7 @@ namespace INSTINCT_RETRIEVAL_NS {
 
         static void build_term_query(const TermQuery& term_query, std::string& sql) {
             sql += term_query.name();
-            sql += "=";
+            sql += " = ";
             build_term_value(term_query.term(), sql);
 
         }
@@ -65,32 +125,48 @@ namespace INSTINCT_RETRIEVAL_NS {
             std::vector<std::string> predicates;
             if (bool_query.must_size()>0) {
                 std::string predicate;
-                for(int i=0; i< bool_query.must_size();++i) {
+                if (const auto n = bool_query.must_size(); n>1) {
                     predicate+= "(";
-                    build_search_query(bool_query.must(i), predicate);
-                    predicate += i==bool_query.must_size()-1 ?  ")" : ") AND ";
+                    for(int i=0; i< n;++i) {
+                        build_search_query(bool_query.must(i), predicate);
+                        predicate += i==n-1 ?  "" : " AND ";
+                    }
+                    predicate += ")";
+                } else {
+                    build_search_query(bool_query.must(0), predicate);
                 }
                 predicates.push_back(predicate);
             }
             if (bool_query.should_size()>0) {
                 std::string predicate;
-                for(int i=0; i< bool_query.should_size();++i) {
+                if (const auto n = bool_query.should_size(); n>1) {
                     predicate+= "(";
-                    build_search_query(bool_query.should(i), predicate);
-                    predicate += i==bool_query.should_size()-1 ?  ")" : ") OR ";
+                    for(int i=0; i< bool_query.should_size();++i) {
+                        build_search_query(bool_query.should(i), predicate);
+                        predicate += i==n-1 ?  "" : " OR ";
+                    }
+                    predicate += ")";
+                } else {
+                    build_search_query(bool_query.should(0), predicate);
                 }
                 predicates.push_back(predicate);
             }
             if (bool_query.mustnot_size()>0) {
                 std::string predicate;
-                for(int i=0; i< bool_query.mustnot_size();++i) {
-                    predicate+= "(NOT ";
-                    build_search_query(bool_query.mustnot(i), predicate);
-                    predicate += i==bool_query.mustnot_size()-1 ?  ")" : ") AND ";
+                if(const auto n = bool_query.mustnot_size(); n>1) {
+                    predicate+= "(";
+                    for(int i=0; i< bool_query.mustnot_size();++i) {
+                        predicate+= "NOT ";
+                        build_search_query(bool_query.mustnot(i), predicate);
+                        predicate += i==n-1 ?  "" : " AND ";
+                    }
+                    predicate += ")";
+                } else {
+                    predicate += "NOT ";
+                    build_search_query(bool_query.mustnot(0), predicate);
                 }
                 predicates.push_back(predicate);
             }
-            sql += " ";
             sql += StringUtils::JoinWith(predicates, " AND ");
         }
 
@@ -105,7 +181,7 @@ namespace INSTINCT_RETRIEVAL_NS {
                 }
             }
             if (!sort_strings.empty()) {
-                sql += " ORDER BY ";
+                sql += "ORDER BY ";
                 sql += StringUtils::JoinWith(sort_strings, ", ");
             }
         }
@@ -120,32 +196,41 @@ namespace INSTINCT_RETRIEVAL_NS {
             const std::vector<Sorter>& sorters = {},
             const int offset = -1,
             const int limit = -1) {
-            std::string sql = "SELECT ";
-            sql += column_list;
-            sql += " FROM ";
-            sql += table_name;
-            sql += " WHERE ";
-            details::build_search_query(search_query, sql);
-            details::build_sorters(sorters, sql);
+            std::vector<std::string> parts = {"SELECT", column_list, "FROM", table_name};
+            if (search_query.query_case() != SearchQuery::QUERY_NOT_SET) {
+                parts.emplace_back("WHERE");
+                std::string sql;
+                details::build_search_query(search_query, sql);
+                parts.push_back(sql);
+            }
+            if (!sorters.empty()) {
+                std::string sql;
+                details::build_sorters(sorters, sql);
+                parts.push_back(sql);
+            }
+
             if (limit>0) {
-                sql += " LIMIT ";
-                sql += std::to_string(limit);
+                parts.emplace_back("LIMIT");
+                parts.emplace_back(std::to_string(limit));
             }
             if (offset>0) {
-                sql += " OFFSET ";
-                sql += std::to_string(offset);
+                parts.emplace_back("OFFSET");
+                parts.push_back(std::to_string(offset));
             }
-            return sql;
+            return StringUtils::JoinWith(parts, " ") + ";";
         }
 
         static std::string ToDeleteString(
             const std::string& table_name,
             const SearchQuery& search_query) {
-            std::string sql = "DELETE FROM ";
-            sql += table_name;
-            sql += " WHERE ";
-            details::build_search_query(search_query, sql);
-            return sql;
+            std::vector<std::string> parts = {"DELETE", "FROM",  table_name};
+            if (search_query.query_case() != SearchQuery::QUERY_NOT_SET) {
+                parts.emplace_back("WHERE");
+                std::string sql;
+                details::build_search_query(search_query, sql);
+                parts.push_back(sql);
+            }
+            return StringUtils::JoinWith(parts, " ") + ";";
         }
     };
 }
