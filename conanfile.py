82b8b6250ea9472c7fc1572dfe4d9e546877c9d8
RobinQu <robinqu@gmail.com>
2024-06-25 09:25:59 +0800

    chores: add options for conan build

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,43 @@
+import os
+
 from conan import ConanFile
 from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain, CMake
+from conan.tools.files import copy, get
 
 
 class InstinctCppRecipe(ConanFile):
     name = "instinct-cpp"
     version = "0.1.5"
     settings = "os", "compiler", "build_type", "arch"
+    no_copy_source = True
+    options = {
+        # module-level switches (instinct-proto and instinct-core cannot be switched off)
+        "with_transformer_module": [True, False],
+        "with_server_module": [True, False],
 
+        # target-level switches
+        "with_duckdb": [True, False],
+        "with_exprtk": [True, False],
+        "with_pdfium": [True, False],
+        "with_duckx": [True, False],
+    }
+
+    default_options = {
+        "with_transformer_module": True,
+        "with_server_module": True,
+        "with_duckdb": True,
+        "with_exprtk": True,
+        "with_pdfium": True,
+        "with_duckx": True
+    }
 
     @property
     def _min_cppstd(self):
         return "20"
+
+    def package_id(self):
+        self.info.clear()
 
     def validate(self):
         check_min_cppstd(self, 20)
@@ -22,7 +48,10 @@ class InstinctCppRecipe(ConanFile):
     def requirements(self):
         self.requires("hash-library/8.0")
         self.requires("bshoshany-thread-pool/4.1.0")
-        self.requires("duckdb/0.10.2", options={"with_httpfs": True})
+
+        if self.options.with_duckdb:
+            self.requires("duckdb/0.10.2", options={"with_httpfs": True})
+
         self.requires("uriparser/0.9.7")
         self.requires("crossguid/0.2.2")
         self.requires("protobuf/5.27.0")
@@ -34,14 +63,21 @@ class InstinctCppRecipe(ConanFile):
         self.requires("nlohmann_json/3.11.3")
         self.requires("base64/0.5.2")
         self.requires("libcurl/8.6.0")
-        self.requires("cpp-httplib/0.15.3")
-        self.requires("tesseract/5.3.3")
-        self.requires("pdfium/95.0.4629")
-        self.requires("duckx/1.2.2")
-        self.requires("exprtk/0.0.2")
         self.requires("inja/3.4.0")
         self.requires("concurrentqueue/1.0.4")
         self.requires("cpptrace/0.5.4")
+
+        if self.options.with_pdfium:
+            self.requires("pdfium/95.0.4629")
+
+        if self.options.with_duckx:
+            self.requires("duckx/1.2.2")
+
+        if self.options.with_exprtk:
+            self.requires("exprtk/0.0.2")
+
+        if self.options.with_transformer_module:
+            self.requires("cpp-httplib/0.15.3")
 
         # deps of examples
         self.requires("cli11/2.4.1")
@@ -60,8 +96,13 @@ class InstinctCppRecipe(ConanFile):
         cmake.build()
 
     def package(self):
-        cmake = CMake(self)
-        cmake.install()
+        # cmake = CMake(self)
+        # cmake.install()
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "LICENSE"))
+        copy(self, "*",
+             src=os.path.join(self.package_folder, "modules/instinct"),
+             dst=os.path.join(self.package_folder, "include", "instinct")
+         )
 
     def package_info(self):
         self.cpp_info.libs = ["hello"]
