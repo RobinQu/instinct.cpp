8120b795f9ca90a2bc51427b80c0fb0e6c4664f9
RobinQu <robinqu@gmail.com>
2024-06-27 23:01:49 +0800

    chores: data_all.hpp

---
+++ modules/instinct-data/include/instinct/data_all.hpp
@@ -0,0 +1,25 @@
+//
+// Created by RobinQu on 2024/6/27.
+//
+
+#ifndef DATA_ALL_HPP
+#define DATA_ALL_HPP
+#include <instinct/data_all.hpp>
+#include <instinct/data_global.hpp>
+#include <instinct/database/base_connection_pool.hpp>
+#include <instinct/database/connection_pool.hpp>
+#include <instinct/database/data_template.hpp>
+#include <instinct/database/database_migration.hpp>
+#include <instinct/database/db_utils.hpp>
+#include <instinct/database/duckdb/duckdb_connection.hpp>
+#include <instinct/database/duckdb/duckdb_connection_pool.hpp>
+#include <instinct/database/duckdb/duckdb_data_template.hpp>
+#include <instinct/database/managed_connection.hpp>
+#include <instinct/object_store/file_system_object_store.hpp>
+#include <instinct/object_store/object_store.hpp>
+#include <instinct/task_scheduler/base_task_scheduler.hpp>
+#include <instinct/task_scheduler/in_process_task_queue.hpp>
+#include <instinct/task_scheduler/task_scheduler.hpp>
+#include <instinct/task_scheduler/thread_pool_task_scheduler.hpp>
+
+#endif //DATA_ALL_HPP
